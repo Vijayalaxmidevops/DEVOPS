d82ddcaecad214bd144ca6557a01e246aff03d61
Vijaya <vijayalaxmi.kagatikar@wipro.com>
2016-06-01 15:40:30 +0530

    Addition Program

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class Main
+{
+	public static void main(String args[])
+	{
+	AddNumbers.main(new String[0]);
+	//SubNumbers.main(new String[0]);
+	// MulNumbers.main(new String[0]);
+	}
+}
